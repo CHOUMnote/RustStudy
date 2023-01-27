ac4c71e69faa4a461460ddee27e811fa68b9befe
KIMJiho <52690007+CHOUMnote@users.noreply.github.com>
2023-01-27 23:48:11 +0900

    study ownership

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,25 +1,51 @@
 /**
- * rust 함수
- * fn function_name(T a,...) -> T{ ... }
- * rust에는 NULL이 없다 따라서 무조건 리턴을 해야한다.
- * ->(리턴문) 생략시 empty tuple 리턴
- * return 키워드는 사용해도 되지만 생략해도 됨 단 ';'는 쓰지 않음
- * 함수 선언은 main 위 아래 어디든 상관 없음
+ * 소유권
+ * 기본적으로 함수 호출시 인자들은 소유권 전달이다.
+ * 따라서 다음 호출시 소유권이 없으므로 사용하지 못한다.
+ * 이러한 문제는 ref, 복사, 재리턴 등 방법들이 있다.
 */
 
-fn function1(){
-    //empty tuple 리턴
-}
-
 fn main() {
-    let a = function1();
-    println!("{:?}",a);
-    println!("{}",function2(10));
+    let name = "Kimjiho".to_owned();
+    println!("{name}"); //정상 작동
+    fnc(name);  //정상 작동 && 소유권 전달
+    //fnc(name);  에러 - name의 소유권이 fnc로 넘어간다. copy가 아님!
     
-    return ()//empty tuple 반환! 
+    //소유권 문제 해결 방법!
+    //방법 1
+    println!("\n===방법 1===");
+    let name = "Kimjiho".to_owned();
+    fnc(name.clone()); //복사 후 전달로 정상 작동
+    fnc(name.clone()); //복사 후 전달로 정상 작동
+    fnc(name.clone()); //복사 후 전달로 정상 작동
+
+    //방법 2
+    println!("\n===방법 2===");
+    let mut t = "Hello World!".to_owned();
+    t = str_return(t);
+    t = str_return(t);
+    t = str_return(t);
+
+    //방법 3
+    println!("\n===방법 3===");
+    let country = "대한민국".to_owned();
+    get_ref(&country);
+    get_ref(&country);
+    get_ref(&country);
 }
 
-fn function2(a:i32) -> i32{
-    let a = a*a;
-    a   //return 생략 + ; 없음
+///소유권을 받는다
+fn fnc(a:String){
+    println!("{a}");
+}
+
+///a넘겨받아서 출력 후 다시 소유권 전달
+fn str_return(a:String) -> String{
+    println!("{a}");
+    a
+}
+
+//call by ref(pointer)
+fn get_ref(a:& String){
+    println!("{a}");
 }

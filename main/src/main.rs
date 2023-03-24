384e6ffd7a53c5125799f290c13733c66c622f7f
Kimjiho <rlawlgh1028@naver.com>
2023-03-24 16:25:47 +0900

    zipping iterator

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,7 +1,23 @@
 /*
 zipping iterator
 */
+
+use std::collections::HashMap;
+
 fn main() {
-    
+    let numbers = vec![1,2,3,4,5,6];
+    let mut strings:Vec<String>=Vec::new();
+    for i in 1..=5{
+        strings.push(i.to_string());
+    }
+
+    print!("i32:stirng hashmap is");
+
+    let mut hashmap:HashMap<i32,String> = numbers
+    .into_iter()
+    .zip(strings.into_iter())
+    .collect(); //갯수가 안맞더라도 남는 원소들은 버려짐
+
+    print!("{hashmap:#?}");
 }
  
